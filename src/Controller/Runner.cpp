fe52a968cc99f00509f924915b2d635e343b7fdb
Alejandro <AlexMJHS@gmail.com>
2016-01-27 09:32:08 -0700

    Add to repo

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,22 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: amag6880
+ */
+#include <iostream> //Using input and output streams.
+using namespace std; //Access to the Standard library.
+
+int main()
+{
+	cout << "This is how you type to the console in C++" << endl;
+	//This would be like System.out.pringln("Hello");
+	int temp;
+
+	cin >> temp;
+
+	cout << temp;
+
+	return 0;
+}
+
